98d67054692819c265396765af6f8a1a352581d8
Christian Briones <cwbriones@gmail.com>
2017-04-10 23:05:42 -0700

    cleaning up shop, update init.sh to reflect recent changes
    Remove everything under syntax/* since they've been handled by their
    respective plugins. Some other files are remnants of a pre-pathogen
    era that remained.
    
    Leave some plugins uninstalled while their usefulness is determined.

--- init.sh
+++ init.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-cd bundle/youcompleteme
-./install.py
+cd bundle/YouCompleteMe
+./install.py --tern-completer --racer-completer --gocode-completer --clang-completer
 cd ../..
 
 ln -s vimrc ../.vimrc
 ln -s vimrc init.vim
+
+ln -s $(pwd) ~/.vim
+mkdir -p ~/.config
+ln -s $(pwd) ~/.config/neovim
