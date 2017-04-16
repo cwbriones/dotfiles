f4ffc8a4f7bb12a90e129042e3b696f42428021d
Christian Briones <cwbriones@gmail.com>
2017-04-15 22:22:05 -0700

    include things from work

--- vim/install.sh
+++ vim/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euo pipefail
-IFS=$'\n\t'
+# IFS=$'\n\t'
 
 # Run the plug install process
 vim +PlugUpdate +qall
@@ -10,9 +10,12 @@ vimdir="$(dirname "$0")"
 ycmdir="$vimdir/vim.symlink/bundle/YouCompleteMe"
 
 # Symlink for neovim ourselves
-mkdir "$HOME/.config"
+mkdir -p "$HOME/.config"
 ln -sf "$vimdir/vim.symlink" "$HOME/.config/nvim"
 
 echo "Installing YouCompleteMe"
-# /usr/bin/env python $ycmdir/install.py \
-#     --tern-completer --racer-completer --gocode-completer --clang-completer
+/usr/bin/env python $ycmdir/install.py \
+    --tern-completer \
+    --racer-completer \
+    --gocode-completer \
+    --clang-completer
